9c15d3f3dc6cebd9aea107b0e5f0c6e8df702c7f
Ian Heraty <heratyian@gmail.com>
2024-07-02 16:00:56 -0500

    Add published_at to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,22 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  body         :text
+#  published_at :datetime
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 class Post < ApplicationRecord
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :unpublished, -> { where("published_at > ?", Time.current).or(where(published_at: nil)) }
+
+  def published?
+    self.published_at.present? && self.published_at <= Time.current
+  end
+
+  def unpublished?
+    self.published_at.nil? || self.published_at > Time.current
+  end
 end
---
+++ db/migrate/20240702182755_add_published_at_to_posts.rb
@@ -0,0 +1,5 @@
+class AddPublishedAtToPosts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :posts, :published_at, :datetime
+  end
+end
