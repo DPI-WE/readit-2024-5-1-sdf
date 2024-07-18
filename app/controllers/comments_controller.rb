01e52555b553877f89151053b26b83d3a841e900
Ian Heraty <heratyian@gmail.com>
2024-07-18 14:20:41 -0500

    Ajaxify creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,6 +27,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @comment.commentable, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
