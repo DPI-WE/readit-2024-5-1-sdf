f51fd99f3b1f56a123236aae89c6fb30376a0ff2
Ian Heraty <heratyian@gmail.com>
2024-08-08 16:35:39 -0500

    Add handling for post meta tag image path

--- app/models/concerns/post/meta_taggable.rb
+++ app/models/concerns/post/meta_taggable.rb
@@ -2,10 +2,23 @@ module Post::MetaTaggable
   extend ActiveSupport::Concern
 
   def to_meta_tags
-    # TODO: add author avatar image
     MetaTagService.defaults.deep_merge({
       title: title,
-      description: body.truncate_words(20)
+      description: body.truncate_words(20),
+      image: image_path,
+      og: {
+        title: title,
+        image: image_path,
+        description: body.truncate_words(20),
+      }
     })
+  end
+
+  def image_path
+    if author.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(author.avatar)
+    else
+      ActionController::Base.helpers.asset_url('default_avatar.png')
+    end
   end
 end
